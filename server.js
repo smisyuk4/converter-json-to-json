e2d6aa03d821710a780757898e343a2e220b3423
Sergiy Misyuk <51460801+smisyuk4@users.noreply.github.com>
2023-05-09 23:18:19 +0300

    write functions to read/convert/save file

---
+++ server.js
@@ -0,0 +1,82 @@
+const fs = require('fs').promises;
+const path = require('path');
+
+// const NAME_FILE = 'react-quiz.json'
+// const NAME_FILE = 'javaScript-quiz.json'
+const NAME_FILE = 'html-css-quiz.json'
+
+const pathFile = path.join(__dirname, 'oldFiles', NAME_FILE);
+
+const templateObj = (question, answer) => {
+  return {
+    question: {
+      lang: {
+        ua: { text: question, imgUrl: '' },
+        eng: { text: '', imgUrl: '' },
+      },
+    },
+    answers: [
+      {
+        isCorrect: true,
+        lang: {
+          ua: { text: answer, imgUrl: '' },
+          eng: { text: '', imgUrl: '' },
+        },
+      },
+      {
+        isCorrect: false,
+        lang: {
+          ua: { text: '', imgUrl: '' },
+          eng: { text: '', imgUrl: '' },
+        },
+      },
+      {
+        isCorrect: false,
+        lang: {
+          ua: { text: '', imgUrl: '' },
+          eng: { text: '', imgUrl: '' },
+        },
+      },
+      {
+        isCorrect: false,
+        lang: {
+          ua: { text: '', imgUrl: '' },
+          eng: { text: '', imgUrl: '' },
+        },
+      },
+    ],
+  };
+};
+
+const readFile = async pathFile => {
+  try {
+    const data = await fs.readFile(pathFile);
+    const json = data.toString();
+    return JSON.parse(json);
+  } catch (err) {
+    console.log(err.message);
+  }
+};
+
+const convertFile = async pathFile => {
+  const oldFile = await readFile(pathFile);
+
+  const newFile = oldFile.map(({ question, answer }) =>
+    templateObj(question, answer)
+  );
+  return newFile;
+};
+
+const saveFile = async () => {
+  const res = await convertFile(pathFile);
+  const json = JSON.stringify(res)
+
+  const newPath = path.join(__dirname, 'newFiles', `new-${NAME_FILE}`);
+  try {
+    fs.writeFile(newPath, json)
+  } catch (err) {
+    console.log(err.message);
+  }
+};
+
+saveFile();
